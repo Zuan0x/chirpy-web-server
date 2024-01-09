27931c34b5bc230959b4902d00762927645f2a83
Zuan0x <zuan.0x@gmail.com>
2024-01-09 16:48:46 +0000

    Add readiness endpoint

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ func main() {
 	//Serve index.html
 	filepathRoot := "."
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	//Serve healthz, return header`200 OK` if server is running
+	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}))
 
 	corsMux := middlewareCors(mux)
 
