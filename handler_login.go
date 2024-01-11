22be17bd5c2e34a2830c421eb416a68b8e025ab0
Zuan0x <zuan.0x@gmail.com>
2024-01-11 12:48:41 +0000

    Add JWT

--- handler_login.go
+++ handler_login.go
@@ -5,25 +5,25 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 	"github.com/Zuan0x/chirpy-web-server/internal/auth"
-	"fmt"
 )
-
 
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Password string `json:"password"`
 		Email    string `json:"email"`
+		ExpiresInSeconds int `json:"expires_in_seconds"`
 	}
+
 	type response struct {
 		User
+		Token string `json:"token"`
 	}
-
+	
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
-
-	fmt.Println("params: ", params)
 
 	err := decoder.Decode(&params)
 	if err != nil {
@@ -43,10 +43,24 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	defaultExpiration := 60 * 60 * 24
+	if params.ExpiresInSeconds == 0 {
+		params.ExpiresInSeconds = defaultExpiration
+	} else if params.ExpiresInSeconds > defaultExpiration {
+		params.ExpiresInSeconds = defaultExpiration
+	}
+
+	token, err := auth.MakeJWT(user.ID, cfg.jwtSecret, time.Duration(params.ExpiresInSeconds)*time.Second)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create JWT")
+		return
+	}
+
 	respondWithJSON(w, http.StatusOK, response{
 		User: User{
 			ID:    user.ID,
 			Email: user.Email,
 		},
+		Token: token,
 	})
 }
--- main.go
+++ main.go
@@ -4,19 +4,28 @@ import (
 	"log"
 	"net/http"
 	"flag"
+	"os"
 
 	"github.com/Zuan0x/chirpy-web-server/internal/database"
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 type apiConfig struct {
 	fileserverHits int
 	DB             *database.DB
+	jwtSecret      string
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	godotenv.Load()
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
 
 	db, err := database.NewDB("database.json")
 	if err != nil {
@@ -35,6 +44,7 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 		DB:             db,
+		jwtSecret:      jwtSecret,
 	}
 
 	router := chi.NewRouter()
@@ -48,6 +58,7 @@ func main() {
 
 	apiRouter.Post("/login", apiCfg.handlerLogin)
 	apiRouter.Post("/users", apiCfg.handlerUsersCreate)
+	apiRouter.Put("/users", apiCfg.handlerUsersUpdate)
 
 	apiRouter.Post("/chirps", apiCfg.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiCfg.handlerChirpsRetrieve)
